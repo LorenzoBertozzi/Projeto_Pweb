35e02461a611b68476a97827467126fea0c10ea7
Xoriz <37419347+ReiDaBatata@users.noreply.github.com>
2023-09-22 10:41:40 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -49,5 +49,106 @@ for(const feedback of listaFeedbacks){
     }
 }
 
+const listaCarousel = [
+    {
+        id : 0 ,
+        srcImage : 'assets/sup1.jpg',
+        altImage : 'Proteinas'
+    },
+    {
+        id : 1 ,
+        srcImage : 'assets/sup2.jpg',
+        altImage : 'Creatinas'
+    },
+    {
+        id : 2 ,
+        srcImage : 'assets/sup3.jpg',
+        altImage : 'Vitaminas'
+    },
+    {
+        id : 3,
+        srcImage : 'assets/sup4.jpg',
+        altImage : 'Acessórios'
+    }
+    ,
+    {
+        id : 4,
+        srcImage : 'assets/sup5.jpg',
+        altImage : 'Pre-Treino'
+    }
+];
 
+for(const carousel of listaCarousel){
+    
+    if(carousel.id == 0){
+        const carousel_indicators = 
+        `
+        <button type="button" data-bs-target="#demo" data-bs-slide-to="${carousel.id}" class="active"></button>
+        `
+        document.getElementById("carousel-indicators").innerHTML += carousel_indicators;
+    }else{    
+        const carousel_indicators = 
+        `
+        <button type="button" data-bs-target="#demo" data-bs-slide-to="${carousel.id}" ></button>
+        `
+        document.getElementById("carousel-indicators").innerHTML += carousel_indicators;
+    }
+    
+    if(carousel.id == 0){
+        const carousel_image = 
+        `
+        <div class="carousel-item active">
+            <img src="${carousel.srcImage}" alt="${carousel.altImage}" class="d-block" style="width:100% ; height: 40%;">
+        </div>
+        `
+        document.getElementById("carousel-inner").innerHTML += carousel_image; 
+    }else{    
+        const carousel_image =
+        `
+        <div class="carousel-item">
+            <img src="${carousel.srcImage}" alt="${carousel.altImage}" class="d-block" style="width:100% ; height: 40%;">
+        </div>
+        `
+        document.getElementById("carousel-inner").innerHTML += carousel_image;
+    }
+}
 
+const listaKits = [
+    {
+        srcKit : "kit1.jpg",
+        altKit : "kit1"
+    },
+    {
+        srcKit : "kit2.jpg",
+        altKit : "kit2"
+    },
+    {
+        srcKit : "kit3.jpg",
+        altKit : "kit3"
+    },
+    {
+        srcKit : "kit4.jpg",
+        altKit : "kit4"
+    },
+    {
+        srcKit : "kit5.jpg",
+        altKit : "kit5"
+    },
+    {
+        srcKit : "kit5.jpg",
+        altKit : "kit5"
+    }
+];
+
+for(const kits of listaKits){
+    const cartaoKit = 
+    `
+    <div class="kit">
+        <div class="kit-image">
+            <img src="${kits.srcKit}" alt="${kits.altKit}">
+        </div>
+        <div class="kit-btn"><button> Ver Mais</button></div>
+    </div>
+    `
+    document.getElementById("kits-layout").innerHTML += cartaoKit;
+}
