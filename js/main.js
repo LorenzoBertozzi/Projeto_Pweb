07aee576791c0395cb76bb630d267bfc9dbfcc75
Xoriz <37419347+ReiDaBatata@users.noreply.github.com>
2023-09-28 10:43:02 -0300

    aula 28 set

--- js/main.js
+++ js/main.js
@@ -31,6 +31,16 @@ const listaFeedbacks = [
     }
 ];
 
+if(listaFeedbacks.length > 0){
+    const areaFeedback = 
+    `
+      <h2 style="padding-top: 20px;">FeedBack de quem confia !</h2>
+      <div id="feedback-layout"></div>
+      <p style="padding-bottom: 20px;">Não tem jeito! A honra é maxima, sempre! <br> Foco total naquilo que voce precisa.</p> 
+    `
+    document.getElementById("feedback").innerHTML += areaFeedback;
+}
+
 let limit = 0;
 for(const feedback of listaFeedbacks){
     limit++;
@@ -151,4 +161,89 @@ for(const kits of listaKits){
     </div>
     `
     document.getElementById("kits-layout").innerHTML += cartaoKit;
+}
+
+const dadosEmpresa = [
+    {
+        email : 'algumacoisadasilva@gmail.com',
+        telefone : '(32) 9 1234-1234'
+    }
+];
+
+for(const dados of dadosEmpresa){
+    const atendimento = 
+    `
+        <h6>Atendimento</h6>
+        <p>E-mail : ${dados.email} <br>tel : ${dados.telefone}</p>
+    `
+    document.getElementById("Atendimento").innerHTML += atendimento;
+}
+
+const linksNav = [
+    {
+        id : 1 ,
+        alt: 'Proteinas' ,
+        href:  '../pages/proteina.html'
+    },
+    {
+        id : 2 ,
+        alt: 'Creatina' ,
+        href:  '../pages/creatina.html'
+    },
+    {
+        id : 3 ,
+        alt: 'Vitaminas' ,
+        href:  '../pages/vitaminas.html'
+    },
+    {   
+        id : 4 ,
+        alt: 'Acessórios' ,
+        href:  '../pages/acessorios.html'
+    },
+    {
+        id : 5 ,
+        alt: 'Pré-Treino' ,
+        href:  '../pages/pre-trino.html'
+    }
+];
+
+{       
+    for(const link of linksNav)
+    {
+        const menu =
+        `
+            <li class="nav-item" ><a class="nav-link" href="${link.href}">${link.alt}</a></li>
+        `
+        document.getElementById("celular-nav1").innerHTML += menu;
+    }
+    const menu_footer =
+    `
+        <h6>Menu</h6>
+    `
+    document.getElementById("menu-footer").innerHTML += menu_footer;
+    for(const link of linksNav)
+    {
+        const menu_footer =
+        `
+            <a href="${link.href}">${link.alt}</a><br>
+        `
+        document.getElementById("menu-footer").innerHTML += menu_footer;
+    }
+}
+
+const formasPagamento = ['pix','debito','credito','boleto'];
+{
+    const titulo =
+    `
+        <p>Formas de Pagamento :</p>
+    `    
+    document.getElementById("forma-de-pagamento").innerHTML += titulo;
+
+    for(const forma of formasPagamento){
+        const printForma =
+        `
+            <p style="text-transform: capitalize;">${forma}</p>   
+        `
+        document.getElementById("forma-de-pagamento").innerHTML += printForma;
+    }
 }
